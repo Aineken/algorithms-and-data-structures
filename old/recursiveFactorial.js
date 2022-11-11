e4f2de6b4816eaa7e1f4ea25cb3545bb3dbd453e
andrzul <aineken93@gmail.com>
2022-11-11 19:02:33 +0100

    recursiveFactorial challenge done

--- old/recursiveFactorial.js
+++ old/recursiveFactorial.js
@@ -1,13 +1,10 @@
 function factorial(n) {
   if (n <= 0) {
-    return null;
-  } else if (n === 1) {
     return 1;
   }
-
-  return factorial(n) * factorial(n - 1);
+  return factorial(n - 1) * n;
 }
 
 console.log(factorial(1));
 console.log(factorial(2));
-console.log(factorial(3));
+console.log(factorial(10));
