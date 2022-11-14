2123ef9bd62c20db5b3984f026a8c96f57099d9f
andrzul <aineken93@gmail.com>
2022-11-14 12:19:47 +0100

    insertion sorting challenge completed

--- algorithm/sortings/bubbleSort.js
+++ algorithm/sortings/bubbleSort.js
@@ -5,9 +5,9 @@ function buubleSort(arr) {
     swapped = false;
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
-        let current = arr[i + 1];
+        let temp = arr[i + 1];
         arr[i + 1] = arr[i];
-        arr[i] = current;
+        arr[i] = temp;
         swapped = true;
       }
     }
