e838d223396225632cf5c71a9f0a29efefb91ad5
andrzul <aineken93@gmail.com>
2022-11-14 15:20:11 +0100

    cartesianProduct challenge completed

---
+++ algorithm/exercises /cartesianProduct.js
@@ -0,0 +1,15 @@
+function cartesianProcust(a, b) {
+  let result = [];
+  for (let i = 0; i < a.length; i++) {
+    let productOne = a[i];
+    for (let j = 0; j < b.length; j++) {
+      let productOTwo = b[j];
+      result.push([productOne, productOTwo]);
+    }
+  }
+  return result;
+}
+
+let a = [2, 4, 5];
+let b = [3, 32];
+console.log(cartesianProcust(a, b));
