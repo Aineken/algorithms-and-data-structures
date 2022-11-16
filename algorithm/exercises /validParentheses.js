6c02a56f0be4b13f4b16bad3f0c184dc848f894b
andrzul <aineken93@gmail.com>
2022-11-16 22:06:27 +0100

    isValid function solution

--- algorithm/exercises /validParentheses.js
+++ algorithm/exercises /validParentheses.js
@@ -1,21 +1,36 @@
-var isValid = function (sub) {
-  if (sub.length % 2 !== 0) {
-    console.log("are you moking on me?");
-    return false;
-  }
-  let container = [];
-
-  for (let i = 0; i < sub.length / 2; i++) {
-    switch (sub[i]) {
-      case "(":
-        break;
-      case "{":
-        break;
-      case "[":
-        break;
+var isValid = function (s) {
+  var stack = [];
+  var len = s.length;
+  var map = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+  for (var i = 0; i < len; i++) {
+    if (stack.length > 0 && map[stack[stack.length - 1]] === s[i]) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
     }
   }
-  return true;
+  return stack.length === 0;
+  //   if (sub.length % 2 !== 0) {
+  //     console.log("are you moking on me?");
+  //     return false;
+  //   }
+  //   let container = [];
+
+  //   for (let i = 0; i < sub.length / 2; i++) {
+  //     switch (sub[i]) {
+  //       case "(":
+  //         break;
+  //       case "{":
+  //         break;
+  //       case "[":
+  //         break;
+  //     }
+  //   }
+  //   return true;
 };
 
 console.log(isValid("{}"));
