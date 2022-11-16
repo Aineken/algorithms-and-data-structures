0c9a93b891a1c1e7b1a22bd2354bdf3dc326b883
andrzul <aineken93@gmail.com>
2022-11-16 22:08:13 +0100

    new challenge accepted

---
+++ algorithm/exercises /removeDuplicates.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  const unic = [];
+
+  for (let i = 0; i < nums.length; i++) {}
+};
+
+console.log(removeDuplicates("{}"));
